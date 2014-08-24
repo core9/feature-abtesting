efcb0b9237e6ac1869fb6f263b60bc0f05cc6531
jessec <robertjessecooke@gmail.com>
2014-08-24 10:09:47 +0200

    cc

--- accounts/localhost/test.js
+++ accounts/localhost/test.js
@@ -12,23 +12,10 @@ function ready(func) {
 	}, ms);
 }
 
-var domReady = function (f) {
-    (/complete|loaded|interactive/.test(document.readyState)) ?
-        f() :
-        setTimeout(domReady, 0, f);
-  };
-
 var myGetElementByXpath = function(path) {
 	return document.evaluate(path, document, null, 9, null).singleNodeValue;
 }
 
-
-
-
 ready(function() {
-	//myGetElementByXpath('/html/body/div/div[3]/div/div/div/div/div[3]/a/span').innerHTML = "test blue";
-});
-
-domReady(function() { // slower
-	myGetElementByXpath('/html/body/div/div[3]/div/div/div/div/div[3]/a/span').innerHTML = "test red";
+	myGetElementByXpath('/html/body/div/div[3]/div/div/div/div/div[3]/a/span').innerHTML = "test blue";
 });
