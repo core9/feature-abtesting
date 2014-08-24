beb235d40c2ccf0fff5a5c61e6540a53a3dd2f36
jessec <robertjessecooke@gmail.com>
2014-08-24 10:07:50 +0200

    cc

--- accounts/localhost/test.js
+++ accounts/localhost/test.js
@@ -12,6 +12,12 @@ function ready(func) {
 	}, ms);
 }
 
+var domReady = function (f) {
+    (/complete|loaded|interactive/.test(document.readyState)) ?
+        f() :
+        setTimeout(domReady, 0, f);
+  };
+
 var myGetElementByXpath = function(path) {
 	return document.evaluate(path, document, null, 9, null).singleNodeValue;
 }
@@ -20,5 +26,9 @@ var myGetElementByXpath = function(path) {
 
 
 ready(function() {
-	myGetElementByXpath('/html/body/div/div[3]/div/div/div/div/div[3]/a/span').innerHTML = "test blue";
+	//myGetElementByXpath('/html/body/div/div[3]/div/div/div/div/div[3]/a/span').innerHTML = "test blue";
+});
+
+domReady(function() { // slower
+	myGetElementByXpath('/html/body/div/div[3]/div/div/div/div/div[3]/a/span').innerHTML = "test red";
 });
