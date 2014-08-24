3b9aed3daac446a44e95d22a635a3456d6369215
jessec <robertjessecooke@gmail.com>
2014-08-24 05:57:17 +0200

    cc

--- accounts/localhost/test.js
+++ accounts/localhost/test.js
@@ -86,11 +86,11 @@ var myGetElementByXpath = function(path) {
 
 
 docReady(function() {
-	myGetElementByXpath('/html/body/div/div[3]/div/div/div/div/div[3]/a/span').innerHTML = "testdsdafs";
+	document.evaluate('/html/body/div/div[3]/div/div/div/div/div[3]/a/span', document, null, 9, null).singleNodeValue.innerHTML = "testdsda";
 });
 
 
 myGetElementByXpath('/html/body/div/div[3]/div/div/div/div/div[3]/a/span').innerHTML = "testdsdafs";
 
 
-
+document.evaluate('/html/body/div/div[3]/div/div/div/div/div[3]/a/span', document, null, 9, null).singleNodeValue.innerHTML = "testdsda";
