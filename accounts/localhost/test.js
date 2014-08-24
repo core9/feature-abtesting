3bf14642fed9e5443ef277af603c26c73ba6a229
jessec <robertjessecooke@gmail.com>
2014-08-24 06:06:55 +0200

    cc

--- accounts/localhost/test.js
+++ accounts/localhost/test.js
@@ -1,46 +1,26 @@
-
 function ready(func) {
-  var span = document.createElement('span');
-  var ms = 0;
-  setTimeout(function() {
-    try {
-      document.body.appendChild(span);
+	var span = document.createElement('span');
+	var ms = 0;
+	setTimeout(function() {
+		try {
+			document.body.appendChild(span);
 
-      document.body.removeChild(span);
+			document.body.removeChild(span);
 
-      //Still here? Then document is ready
-      func();
-    } catch(e) {
-      //Whoops, document is not ready yet, try again...
+			// Still here? Then document is ready
+			func();
+		} catch (e) {
+			// Whoops, document is not ready yet, try again...
 
-      setTimeout(arguments.callee, ms);
-    }
-  }, ms);
+			setTimeout(arguments.callee, ms);
+		}
+	}, ms);
 }
-
 
 var myGetElementByXpath = function(path) {
 	return document.evaluate(path, document, null, 9, null).singleNodeValue;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-ready(function(){
-	document.evaluate('/html/body/div/div[3]/div/div/div/div/div[3]/a/span', document, null, 9, null).singleNodeValue.innerHTML = "testdsda";
-	
+ready(function() {
 	myGetElementByXpath('/html/body/div/div[3]/div/div/div/div/div[3]/a/span').innerHTML = "test ok";
-
 });
