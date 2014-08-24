93458470b987ec8b18569a538dfc82d2698aa610
jessec <robertjessecooke@gmail.com>
2014-08-24 06:05:04 +0200

    cc

--- accounts/localhost/test.js
+++ accounts/localhost/test.js
@@ -1,4 +1,24 @@
 
+function ready(func) {
+  var span = document.createElement('span');
+  var ms = 0;
+  setTimeout(function() {
+    try {
+      document.body.appendChild(span);
+
+      document.body.removeChild(span);
+
+      //Still here? Then document is ready
+      func();
+    } catch(e) {
+      //Whoops, document is not ready yet, try again...
+
+      setTimeout(arguments.callee, ms);
+    }
+  }, ms);
+}
+
+
 var myGetElementByXpath = function(path) {
 	return document.evaluate(path, document, null, 9, null).singleNodeValue;
 }
@@ -19,4 +39,9 @@ myGetElementByXpath('/html/body/div/div[3]/div/div/div/div/div[3]/a/span').inner
 document.evaluate('/html/body/div/div[3]/div/div/div/div/div[3]/a/span', document, null, 9, null).singleNodeValue.innerHTML = "testdsda";
 
 
-setTimeout(function(){document.evaluate('/html/body/div/div[3]/div/div/div/div/div[3]/a/span', document, null, 9, null).singleNodeValue.innerHTML = "testdsda";}, 3000);
+
+
+
+ready(function(){
+	document.evaluate('/html/body/div/div[3]/div/div/div/div/div[3]/a/span', document, null, 9, null).singleNodeValue.innerHTML = "testdsda";
+});
